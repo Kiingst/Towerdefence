e4cdc55d755749050ee012d306e84aaba6cff25e
Kingston Armstrong <70528667+Kiingst@users.noreply.github.com>
2023-10-29 18:31:33 -0500

    Update tower_builder.gd

--- Core/UI/Tower_Builder/tower_builder.gd
+++ Core/UI/Tower_Builder/tower_builder.gd
@@ -1,5 +1,5 @@
 extends GridContainer
-var base_level = get_tree().get_nodes_in_group("Base_Level")[0]
+#var base_level = get_tree().get_nodes_in_group("Base_Level")[0]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
